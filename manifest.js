241c6efe8cd5da7bd6f7e0adc434d5428bf4c8e0
AtharvPatil-kit <atharvapatil808@gmail.com>
2025-07-28 15:46:45 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "Smart UPI QR Generator",
+  "short_name": "UPI QR",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "orientation": "portrait",
+  "background_color": "#ffffff",
+  "theme_color": "#7c3aed",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
